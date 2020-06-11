11471dd5e0866119a5d50e0a78965c8bdcc21ea4
ian cole <ianccole@gmail.com>
2020-06-11 17:03:18 +0100

    mesh net initial

---
+++ lora-mesh-net/src/MeshNet.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+#include <RHRouter.h>
+#include <RHMesh.h>
+#include <RH_RF95.h>
+#include <MeshNet.h>
+
+
+#define FREQMHZ 434.4
+#define POWER 20
+#define CAD_TIMEOUT 500
+
+RH_RF95 MeshNet::rf95;
+
+MeshNet::MeshNetPingMessage msg;
+
+void MeshNet::setup(uint8_t thisAddress, float freqMHz, int8_t power, uint16_t cad_timeout)
+{
+	MeshNet::power = power;
+	manager = new RHMesh(rf95, thisAddress);
+
+	if (!manager->init())
+	{
+	Serial.println(F("init failed"));
+	return;
+	}
+
+#ifdef LED_BUILTIN
+	pinMode(LED_BUILTIN, OUTPUT);
+#endif
+
+	rf95.setTxPower(power);
+	rf95.setFrequency(freqMHz);
+	rf95.setCADTimeout(cad_timeout);
+
+//   // long range configuration requires for on-air time
+//   boolean longRange = false;
+//   if (longRange)
+//   {
+//     RH_RF95::ModemConfig modem_config = {
+//         0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+//         0xC4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+//         0x08  // Reg 0x26: LowDataRate=On, Agc=Off.  0x0C is LowDataRate=ON, ACG=ON
+//     };
+//     rf95.setModemRegisters(&modem_config);
+//     if (!rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096))
+//     {
+//       Serial.println(F("set config failed"));
+//     }
+//   }
+
+
+}
+
+void MeshNet::loop(uint16_t wait_ms)
+{
+	uint8_t len = sizeof(msg);
+	uint8_t from;
+	if (manager->recvfromAckTimeout((uint8_t *)&msg, &len, wait_ms, &from))
+	{
+		blinkLed();
+	    Serial.println("incoming");
+	}
+}
+
+void MeshNet::pingNode(uint8_t address, uint8_t flags)
+{
+	uint8_t error;
+
+	memset(&msg, 0, sizeof(MeshNet::MeshNetPingMessage));
+
+    msg.header.msgType = MESH_NET_MESSAGE_TYPE_PING_REQUEST;
+	msg.power = power;
+    
+	blinkLed();
+	Serial.println("outgoing");
+	error = manager->sendtoWait((uint8_t*)&msg, sizeof(MeshNet::MeshNetPingMessage), address, flags);
+
+    switch(error)
+    {
+        case RH_ROUTER_ERROR_NO_ROUTE:
+            Serial.println("no route");
+            break;
+
+        case RH_ROUTER_ERROR_NONE:
+        {
+            int16_t lastRSSI;
+            int lastSNR;
+
+            RHRouter::RoutingTableEntry *route = manager->getRouteTo(address);
+            lastRSSI = rf95.lastRssi();
+            lastSNR = rf95.lastSNR();
+        }
+        break;
+    }
+}
--- lora-mesh-net/src/MeshNet.h
+++ lora-mesh-net/src/MeshNet.h
@@ -3,21 +3,20 @@
 #define MeshNet_h
 
 // Types of MeshNet message, used to set msgType in the MeshNetHeader
-#define MESH_NET_MESSAGE_TYPE_APPLICATION                    0
+#define MESH_NET_MESSAGE_TYPE_ROUTE_FAILURE                  0
 #define MESH_NET_MESSAGE_TYPE_PING_REQUEST                   1
 #define MESH_NET_MESSAGE_TYPE_PING_RESPONSE                  2
-#define MESH_NET_MESSAGE_TYPE_ROUTE_FAILURE                  3
 
-class MeshNet : public RHMesh
+class MeshNet
 {
 public:
-    #define MESH_NET_MAX_MESSAGE_LEN (RH_ROUTER_MAX_MESSAGE_LEN - sizeof(MeshNet::MeshMessageHeader))
+    #define MESH_NET_MAX_MESSAGE_LEN (RH_MESH_MAX_MESSAGE_LEN - sizeof(MeshNet::MeshMessageHeader))
 
     /// Structure of the basic MeshNet header.
     typedef struct
     {
         uint8_t             msgType;  ///< Type of MeshNet message, one of MESH_NET_MESSAGE_TYPE_*
-    } MeshNetMessageHeader;
+    } MeshMessageHeader;
 
     /// Signals an application layer message for the caller of RHMesh
     typedef struct
@@ -29,12 +28,43 @@ public:
     typedef struct
     {
         MeshMessageHeader   header;  ///< msgType = RH_MESH_MESSAGE_TYPE_ROUTE_DISCOVERY_*
-        uint8_t             destlen; ///< Reserved. Must be 1
-        uint8_t             dest;    ///< The address of the destination node being pinged
         uint8_t             power;   ///< Transmit power of this message
         uint8_t             rssi;    ///< RSSI in Ping response
         uint8_t             snr;     ///< SNR in Ping response
     } MeshNetPingMessage;
+
+    void setup(uint8_t thisAddress, float freqMHz, int8_t power, uint16_t cad_timeout);
+
+    void loop(uint16_t wait_ms);
+
+    void pingNode(uint8_t address, uint8_t flags = 0);
+
+    int16_t lastRssi()
+    {
+        return rf95.lastRssi();
+    }
+
+    void printRoutingTable()
+    {
+        manager->printRoutingTable();
+    }
+
+    void blinkLed()
+    {
+        #ifdef LED_BUILTIN
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        #endif
+    };
+
+private:
+    int8_t power;
+    // Singleton instance of the radio driver
+    static RH_RF95 rf95;
+
+    // Class to manage message delivery and receipt, using the driver declared above
+    RHMesh *manager;
 
 
 };
